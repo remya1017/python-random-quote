97f67d33b555813a44fa3a71cdcb9132eb275857
remya1017 <remya1017@github.com>
2020-05-22 05:06:02 +0000

    Read lines task

--- get-quote.py
+++ get-quote.py
@@ -3,12 +3,16 @@ def primary():
   #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
-  quotes = f.readlines()
+  while True:
+    quotes = f.readline()
+    if not quotes:
+      break
+    print(quotes)
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes [rnd])
+  #last = 13
+  #rnd = random.randint(0, last)
+  print(quotes)
 
 if __name__== "__main__":
   primary()
